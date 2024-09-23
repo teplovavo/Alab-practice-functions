8ecebf6e2edd8adc9e51f8563383b81a74df66b7
Veronika Teplova <teplovavo@gmail.com>
2024-09-23 14:09:23 -0700

    sum in array

--- script.js
+++ script.js
@@ -8,13 +8,30 @@
 //////////--------------------------------part 1 ------------------------------------
 console.log("Part 1")
 /*
-Take an array of numbers and return the sum.
+
 Take an array of numbers and return the average.
 Take an array of strings and return the longest string.
 Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
 For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
 Take a number, n, and print every number between 1 and n without using loops. Use recursion.
 */
+
+//Take an array of numbers and return the sum.
+function sumArray (arr) {
+    return arr.reduce((sum, num) => sum + num, 0);
+}
+const numbers = [1, 2, 3, 4];
+console.log(sumArray(numbers));
+
+
+
+
+
+
+
+
+
+
 
 //////////--------------------------------part 2 ------------------------------------
 console.log("Part 2")
