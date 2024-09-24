3a9b7cee54601aa25b5a72113981429834359ff9
Veronika Teplova <teplovavo@gmail.com>
2024-09-23 18:05:44 -0700

    String

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ console.log("Part 1")
 /*
 
 
-Take an array of strings and return the longest string.
+
 Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
 For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
 Take a number, n, and print every number between 1 and n without using loops. Use recursion.
@@ -30,8 +30,12 @@ function avArray(arr) {
 }
 console.log("Av: ", avArray(numbers));
 
-
-
+//Take an array of strings and return the longest string.
+function strArray(arr) {
+    return arr.reduce((longest, current) => current.length > longest.length ? current : longest, "");
+}
+const strings = ["Sofa", "Stool", "Chair", "Credenza", "Bed"];
+console.log("Str: ", strArray(strings));
 
 
 
